75910aa762ddfe458bfda5083bcb67c0d689b297
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-04-11 18:30:42 -0400

    Adjusted Quiz question and answer formats for easier manipulation. Small changes to HTML and CSS formatting

--- assets/script.js
+++ assets/script.js
@@ -1,161 +1,39 @@
+var questionSet = document.querySelectorAll('li');
+var question
+
+
+
 var timer = document.querySelector('#timer');
 timer.append('placeholder');
 
 
 
-
-var qSet1 = [
+// setting up questions and answers.
+var questionAnswers = [
     {
-        question: "What does HTML stand for?"
-    },
-    {
-        name: "He Tells Me Langugage",
-        correct: false,
+        question: "What does HTML stand for?",
+        answerset: ["He Tells Me Langugage", "Hello Timmy More Lasagna", "HyperText Markup Language","HyperTest Markup sLang"],
+        answer: "Hypertext Markup Language",
     },
 
     {
-        name: "Hello Timmy More Lasagna",
-        correct: false,
-    },
-
-    {
-        name: "HyperText Markup Language",
-        correct: true,
-    },
-
-    {
-        name: "HyperTest Markup sLang",
-        correct: false,
-    }
-]
-
-var qSet2 = [
-    {
-        question: "",
-    },
-
-    {
-        name: "He Tells Me Langugage",
-        correct: false,
-    },
-
-    {
-        name: "Hello Timmy More Lasagna",
-        correct: false,
-    },
-
-    {
-        name: "HyperText Markup Language",
-        correct: true,
-    },
-
-    {
-        name: "HyperTest Markup sLang",
-        correct: false,
-    }
-]
-
-var qSet3 = [
-    {
-        question: "",
-    },
-
-    {
-        name: "He Tells Me Langugage",
-        correct: true,
-    },
-
-    {
-        name: "Hello Timmy More Lasagna",
-        correct: false,
-    },
-
-    {
-        name: "HyperText Markup Language",
-        correct: false,
-    },
-
-    {
-        name: "HyperTest Markup sLang",
-        correct: false,
-    }
-]
-
-
-var qSet4 = [
-    {
-        question: "",
-    },
-
-    {
-        name: "He Tells Me Langugage",
-        correct: false,
-    },
-
-    {
-        name: "Hello Timmy More Lasagna",
-        correct: false,
-    },
-
-    {
-        name: "HyperText Markup Language",
-        correct: true,
-    },
-
-    {
-        name: "HyperTest Markup sLang",
-        correct: false,
-    }
-]
-
-var qSet5 = [
-    {
-        question: "",
-    },
-    
-    {
-        name: "He Tells Me Langugage",
-        correct: false,
-    },
-
-    {
-        name: "Hello Timmy More Lasagna",
-        correct: false,
-    },
-
-    {
-        name: "HyperText Markup Language",
-        correct: true,
-    },
-
-    {
-        name: "HyperTest Markup sLang",
-        correct: false,
-    }
-]
-
-var qSet6 = [
-    {
-        question: "",
-    },
-    
-    {
-        name: "He Tells Me Langugage",
-        correct: false,
-    },
-
-    {
-        name: "Hello Timmy More Lasagna",
-        correct: false,
-    },
-
-    {
-        name: "HyperText Markup Language",
-        correct: true,
-    },
-
-    {
-        name: "HyperTest Markup sLang",
-        correct: false,
-    }
+        question: ".querySelector is used to effect elements in what programming language?",
+        answerset: ["Javascript", "Java", "CSS", "Python"],
+        answer: "Javascript"
+   },
+   {
+        question: "When linking a Javascript file to an HTML file, where does it belong on the page?", 
+        answerset: ["Within the elements you want to affect with Javascript", "Between the end of the main element, and the end of the html element", "In the head element", "At the bottom of the main element"],
+        answer: "At the bottom of the main element"
+   },
+   {
+        question: "What is best practice for naming variables?",
+        answerset: ["long and descriptive names for what each variable is going to be used for.", "CamelCase", "Use the initials for what the variable is going to be used for.",  "Whatever you want."],
+        answer: "CamelCase",
+   },
+   {
+        question: "How are arrays indicated in Javascript?",
+        answerset: ["Curly braces - {}", "Brackets - []", "Parenthesis - () ", "Quotation marks - ''"],
+        answer: "Brackets - []"
+   },
 ]
