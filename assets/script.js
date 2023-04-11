a820ef3f230c30624ad7e1df3269710bd8827d53
Aubrey McKinney <99014955+Shadowasders@users.noreply.github.com>
2023-04-11 14:57:05 -0400

    added reset.css, added basis for quiz questions

---
+++ assets/script.js
@@ -0,0 +1,92 @@
+var questionPackage
+
+
+
+var qSet1 = [
+    {
+        name: "He Tells Me Langugage",
+        correct: false,
+    },
+
+    {
+        name: "Hello Timmy More Lasagna",
+        correct: false,
+    },
+
+    {
+        name: "HyperText Markup Language",
+        correct: true,
+    },
+
+    {
+        name: "HyperTest Markup sLang",
+        correct: false,
+    }
+]
+
+var qSet2 = [
+    {
+        name: "He Tells Me Langugage",
+        correct: false,
+    },
+
+    {
+        name: "Hello Timmy More Lasagna",
+        correct: false,
+    },
+
+    {
+        name: "HyperText Markup Language",
+        correct: true,
+    },
+
+    {
+        name: "HyperTest Markup sLang",
+        correct: false,
+    }
+]
+
+var qSet3 = [
+    {
+        name: "He Tells Me Langugage",
+        correct: true,
+    },
+
+    {
+        name: "Hello Timmy More Lasagna",
+        correct: false,
+    },
+
+    {
+        name: "HyperText Markup Language",
+        correct: false,
+    },
+
+    {
+        name: "HyperTest Markup sLang",
+        correct: false,
+    }
+]
+
+
+var qSet4 = [
+    {
+        name: "He Tells Me Langugage",
+        correct: false,
+    },
+
+    {
+        name: "Hello Timmy More Lasagna",
+        correct: false,
+    },
+
+    {
+        name: "HyperText Markup Language",
+        correct: true,
+    },
+
+    {
+        name: "HyperTest Markup sLang",
+        correct: false,
+    }
+]
